74dad27b2c0a15516cf55253976deed9685574b3
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-08 19:11:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -33,4 +33,10 @@ function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
 }
 
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+
+  return array
+}
+
 
