6f26fdbaffaaee99d0ed239d7423facdab89c88d
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-08 19:08:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
-var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; 
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
 
 function addElementToBeginningOfArray(arr, element) {
   return [element,...arr]; 
